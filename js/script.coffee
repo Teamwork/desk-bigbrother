61859b5bda245094cb5a698d9456cec42d6319b9
Alder Cass <aldercass@teamwork.com>
2017-04-07 15:51:02 +0100

    Login verification

--- js/script.coffee
+++ js/script.coffee
@@ -1,139 +1,140 @@
-init = (APIKey) ->
-	Inbox = (name, id) ->
-		self = this
-		self.name = name
-		self.id = id
-		return
+Inbox = (name, id) ->
+	self = this
+	self.name = name
+	self.id = id
+	return
 
-	User = (name, id, avatar) ->
-		self = this
-		self.name = name
-		self.id = id
-		self.avatar = avatar
-		return
+User = (name, id, avatar) ->
+	self = this
+	self.name = name
+	self.id = id
+	self.avatar = avatar
+	return
 
-	Ticket = (id, agent, preview, subject, status, date, avatar) ->
-		self = this
-		self.id = id
-		self.agent = agent
-		self.preview = preview
-		self.subject = subject
-		self.status = status
-		self.date = date
-		self.avatar = avatar
-		return
+Ticket = (id, agent, preview, subject, status, date, avatar) ->
+	self = this
+	self.id = id
+	self.agent = agent
+	self.preview = preview
+	self.subject = subject
+	self.status = status
+	self.date = date
+	self.avatar = avatar
+	return
 
-	FilterViewModel = () ->
-		self = this
-		self.inboxes = ko.observableArray()
-		self.users = ko.observableArray()
-		self.selectedInboxes = ko.observableArray()
-		self.selectedUsers = ko.observableArray()
-		self.selectedUserIds = ko.observableArray()
-		self.allUserIds = ko.observableArray()
-		self.selectedInboxIds = ko.observableArray()
-		self.inboxCount = ko.observable("0 selected")
-		self.userCount = ko.observable("0 selected")
-		self.tickets = ko.observableArray()
-		self.meAvatar = ko.observable("images/pixel.png")
-		self.meName = ko.observable()
-		self.meEmail = ko.observable()
+FilterViewModel = () ->
+	self = this
+	self.inboxes = ko.observableArray()
+	self.users = ko.observableArray()
+	self.selectedInboxes = ko.observableArray()
+	self.selectedUsers = ko.observableArray()
+	self.selectedUserIds = ko.observableArray()
+	self.allUserIds = ko.observableArray()
+	self.selectedInboxIds = ko.observableArray()
+	self.inboxCount = ko.observable("0 selected")
+	self.userCount = ko.observable("0 selected")
+	self.tickets = ko.observableArray()
+	self.meAvatar = ko.observable("images/pixel.png")
+	self.meName = ko.observable()
+	self.meEmail = ko.observable()
 
-		self.selectInbox = (inbox) ->
-			$("#inboxes a[data-id='#{inbox.id}']").toggleClass "selected"
-			if self.selectedInboxes.indexOf(inbox) > -1
-				self.selectedInboxes.remove(inbox)
-			else
-				self.selectedInboxes.push(inbox)
+	self.selectInbox = (inbox) ->
+		$("#inboxes a[data-id='#{inbox.id}']").toggleClass "selected"
+		if self.selectedInboxes.indexOf(inbox) > -1
+			self.selectedInboxes.remove(inbox)
+		else
+			self.selectedInboxes.push(inbox)
 
-		self.selectUser = (user) ->
-			$("#users a[data-id='#{user.id}']").toggleClass "selected"
-			if self.selectedUsers.indexOf(user) > -1
-				self.selectedUsers.remove(user)
-			else
-				self.selectedUsers.push(user)
+	self.selectUser = (user) ->
+		$("#users a[data-id='#{user.id}']").toggleClass "selected"
+		if self.selectedUsers.indexOf(user) > -1
+			self.selectedUsers.remove(user)
+		else
+			self.selectedUsers.push(user)
 
-		self.filterTickets = () ->
-			$(".placeholder, .tickets, .no-results").stop().animate {opacity: 0}, 200, () ->
+	self.filterTickets = () ->
+		$(".placeholder, .tickets, .no-results").stop().animate {opacity: 0}, 200, () ->
+			$(this).hide()
+		$(".loader").stop().show().animate {opacity: 1}, 200
+		BigBrother.tickets.removeAll()
+		$.ajax({
+			url: "https://digitalcrew.teamwork.com/desk/v1/tickets/search.json",
+			method: "POST",
+			data: {
+				"search": '',
+				"searchType": "content",
+				"inboxIds": BigBrother.selectedInboxIds(),
+				"assignedTo": if BigBrother.selectedUserIds().length then BigBrother.selectedUserIds() else BigBrother.allUserIds(),
+				"pageSize": $("#ticketCount").val(),
+				"page": 1,
+				"sortBy": "updatedAt",
+				"sortDir": "asc",
+				"statuses": ["solved","closed"],
+				"lastUpdated": $("#startDate").val()
+			}
+			headers: {
+				"Authorization": "Basic " + btoa(APIKey + ":xxx")
+			}
+		}).done (data) ->
+			$(".loader").stop().animate {opacity: 0}, 200, () ->
 				$(this).hide()
-			$(".loader").stop().show().animate {opacity: 1}, 200
-			BigBrother.tickets.removeAll()
-			$.ajax({
-				url: "https://digitalcrew.teamwork.com/desk/v1/tickets/search.json",
-				method: "POST",
-				data: {
-					"search": '',
-					"searchType": "content",
-					"inboxIds": BigBrother.selectedInboxIds(),
-					"assignedTo": if BigBrother.selectedUserIds().length then BigBrother.selectedUserIds() else BigBrother.allUserIds(),
-					"pageSize": $("#ticketCount").val(),
-					"page": 1,
-					"sortBy": "updatedAt",
-					"sortDir": "asc",
-					"statuses": ["solved","closed"],
-					"lastUpdated": $("#startDate").val()
-				}
-				headers: {
-					"Authorization": "Basic " + btoa(APIKey + ":xxx")
-				}
-			}).done (data) ->
-				$(".loader").stop().animate {opacity: 0}, 200, () ->
-					$(this).hide()
-				if data.tickets.length is 0
-					$(".no-results").stop().show().animate {opacity: 1}, 200
-				else
-					mappedTickets = $.map data.tickets, (ticket) ->
-						agent = if ticket.assignedTo then ticket.assignedTo.firstName + " " + ticket.assignedTo.lastName else "None"
-						avatar = if ticket.assignedTo then $(".filters a[data-id='#{ticket.assignedTo.id}'] img").attr("src") else null
-						ticket = new Ticket(ticket.id, agent, ticket.preview, ticket.subject, ticket.status, ticket.updatedAt, avatar)
-						BigBrother.tickets.push(ticket)
-						return
-					$(".tickets").stop().show().animate {opacity: 1}, 200
-				return
+			if data.tickets.length is 0
+				$(".no-results").stop().show().animate {opacity: 1}, 200
+			else
+				mappedTickets = $.map data.tickets, (ticket) ->
+					agent = if ticket.assignedTo then ticket.assignedTo.firstName + " " + ticket.assignedTo.lastName else "None"
+					avatar = if ticket.assignedTo then $(".filters a[data-id='#{ticket.assignedTo.id}'] img").attr("src") else null
+					ticket = new Ticket(ticket.id, agent, ticket.preview, ticket.subject, ticket.status, ticket.updatedAt, avatar)
+					BigBrother.tickets.push(ticket)
+					return
+				$(".tickets").stop().show().animate {opacity: 1}, 200
 			return
-
-		self.reviewTickets = (form) ->
-			body = "<h5>What was done well?</h5>"
-			body += "<p>#{$(form).find(".donewell").val()}</p>"
-			body += "<h5>Would you like to suggest any improvements?</h5>"
-			body += "<p>#{$(form).find(".improvements").val()}</p>"
-			body += "<h5>Rating out of 5:</h5>"
-			body += "<p>#{$(form).find(".rating input:checked").val()}</p>"
-			$(form).closest(".ticket").addClass("loading")
-			$.ajax({
-				url: "https://digitalcrew.teamwork.com/desk/v1/tickets/#{$(form).find('.id').val()}.json",
-				method: "POST",
-				data: {
-					"type": "note",
-					"body": body
-				}
-				headers: {
-					"Authorization": "Basic " + btoa(APIKey + ":xxx")
-				}
-			}).done (data) ->
-				$(form).closest(".ticket").removeClass("loading").addClass("success")
-				return
-			return
-
 		return
 
-	BigBrother = new FilterViewModel()
+	self.reviewTickets = (form) ->
+		body = "<h5>What was done well?</h5>"
+		body += "<p>#{$(form).find(".donewell").val()}</p>"
+		body += "<h5>Would you like to suggest any improvements?</h5>"
+		body += "<p>#{$(form).find(".improvements").val()}</p>"
+		body += "<h5>Rating out of 5:</h5>"
+		body += "<p>#{$(form).find(".rating input:checked").val()}</p>"
+		$(form).closest(".ticket").addClass("loading")
+		$.ajax({
+			url: "https://digitalcrew.teamwork.com/desk/v1/tickets/#{$(form).find('.id').val()}.json",
+			method: "POST",
+			data: {
+				"type": "note",
+				"body": body
+			}
+			headers: {
+				"Authorization": "Basic " + btoa(APIKey + ":xxx")
+			}
+		}).done (data) ->
+			$(form).closest(".ticket").removeClass("loading").addClass("success")
+			return
+		return
 
-	BigBrother.selectedInboxes.subscribe (inboxes) ->
-		ids = []
-		$(inboxes).each ->
-			inbox = this
-			ids.push inbox.id
-		BigBrother.inboxCount(ids.length + " selected")
-		BigBrother.selectedInboxIds(ids)
+	return
 
-	BigBrother.selectedUsers.subscribe (users) ->
-		ids = []
-		$(users).each ->
-			user = this
-			ids.push user.id
-		BigBrother.userCount(users.length + " selected")
-		BigBrother.selectedUserIds(ids)
+BigBrother = new FilterViewModel()
+
+BigBrother.selectedInboxes.subscribe (inboxes) ->
+	ids = []
+	$(inboxes).each ->
+		inbox = this
+		ids.push inbox.id
+	BigBrother.inboxCount(ids.length + " selected")
+	BigBrother.selectedInboxIds(ids)
+
+BigBrother.selectedUsers.subscribe (users) ->
+	ids = []
+	$(users).each ->
+		user = this
+		ids.push user.id
+	BigBrother.userCount(users.length + " selected")
+	BigBrother.selectedUserIds(ids)
+
+init = (APIKey) ->
 
 	$.ajax({
 		url: "https://digitalcrew.teamwork.com/desk/v1/me.json",
@@ -144,7 +145,12 @@ init = (APIKey) ->
 		BigBrother.meName(data.user.firstName + " " + data.user.lastName)
 		BigBrother.meEmail(data.user.email)
 		BigBrother.meAvatar(data.user.avatarURL)
+		$(".login").hide()
+		localStorage.setItem("APIKey",$("#APIKey").val())
+		ko.applyBindings BigBrother
 		return
+	.fail ->
+		$(".login p").text "Invalid API Key!"
 
 	$.ajax({
 		url: "https://digitalcrew.teamwork.com/desk/v1/inboxes.json",
@@ -177,13 +183,9 @@ init = (APIKey) ->
 			$(".filters h4").removeClass "open"
 			$(this).addClass "open"
 
-	ko.applyBindings BigBrother
-
 $("#login-form").on "submit", (e) ->
 	e.preventDefault()
 	init $("#APIKey").val()
-	$(".login").hide()
-	localStorage.setItem("APIKey",$("#APIKey").val())
 	return
 
 if localStorage.getItem "APIKey"
--- js/script.js
+++ js/script.js
@@ -1,163 +1,169 @@
-var init;
+var BigBrother, FilterViewModel, Inbox, Ticket, User, init;
 
-init = function(APIKey) {
-  var BigBrother, FilterViewModel, Inbox, Ticket, User;
-  Inbox = function(name, id) {
-    var self;
-    self = this;
-    self.name = name;
-    self.id = id;
+Inbox = function(name, id) {
+  var self;
+  self = this;
+  self.name = name;
+  self.id = id;
+};
+
+User = function(name, id, avatar) {
+  var self;
+  self = this;
+  self.name = name;
+  self.id = id;
+  self.avatar = avatar;
+};
+
+Ticket = function(id, agent, preview, subject, status, date, avatar) {
+  var self;
+  self = this;
+  self.id = id;
+  self.agent = agent;
+  self.preview = preview;
+  self.subject = subject;
+  self.status = status;
+  self.date = date;
+  self.avatar = avatar;
+};
+
+FilterViewModel = function() {
+  var self;
+  self = this;
+  self.inboxes = ko.observableArray();
+  self.users = ko.observableArray();
+  self.selectedInboxes = ko.observableArray();
+  self.selectedUsers = ko.observableArray();
+  self.selectedUserIds = ko.observableArray();
+  self.allUserIds = ko.observableArray();
+  self.selectedInboxIds = ko.observableArray();
+  self.inboxCount = ko.observable("0 selected");
+  self.userCount = ko.observable("0 selected");
+  self.tickets = ko.observableArray();
+  self.meAvatar = ko.observable("images/pixel.png");
+  self.meName = ko.observable();
+  self.meEmail = ko.observable();
+  self.selectInbox = function(inbox) {
+    $("#inboxes a[data-id='" + inbox.id + "']").toggleClass("selected");
+    if (self.selectedInboxes.indexOf(inbox) > -1) {
+      return self.selectedInboxes.remove(inbox);
+    } else {
+      return self.selectedInboxes.push(inbox);
+    }
   };
-  User = function(name, id, avatar) {
-    var self;
-    self = this;
-    self.name = name;
-    self.id = id;
-    self.avatar = avatar;
+  self.selectUser = function(user) {
+    $("#users a[data-id='" + user.id + "']").toggleClass("selected");
+    if (self.selectedUsers.indexOf(user) > -1) {
+      return self.selectedUsers.remove(user);
+    } else {
+      return self.selectedUsers.push(user);
+    }
   };
-  Ticket = function(id, agent, preview, subject, status, date, avatar) {
-    var self;
-    self = this;
-    self.id = id;
-    self.agent = agent;
-    self.preview = preview;
-    self.subject = subject;
-    self.status = status;
-    self.date = date;
-    self.avatar = avatar;
-  };
-  FilterViewModel = function() {
-    var self;
-    self = this;
-    self.inboxes = ko.observableArray();
-    self.users = ko.observableArray();
-    self.selectedInboxes = ko.observableArray();
-    self.selectedUsers = ko.observableArray();
-    self.selectedUserIds = ko.observableArray();
-    self.allUserIds = ko.observableArray();
-    self.selectedInboxIds = ko.observableArray();
-    self.inboxCount = ko.observable("0 selected");
-    self.userCount = ko.observable("0 selected");
-    self.tickets = ko.observableArray();
-    self.meAvatar = ko.observable("images/pixel.png");
-    self.meName = ko.observable();
-    self.meEmail = ko.observable();
-    self.selectInbox = function(inbox) {
-      $("#inboxes a[data-id='" + inbox.id + "']").toggleClass("selected");
-      if (self.selectedInboxes.indexOf(inbox) > -1) {
-        return self.selectedInboxes.remove(inbox);
-      } else {
-        return self.selectedInboxes.push(inbox);
+  self.filterTickets = function() {
+    $(".placeholder, .tickets, .no-results").stop().animate({
+      opacity: 0
+    }, 200, function() {
+      return $(this).hide();
+    });
+    $(".loader").stop().show().animate({
+      opacity: 1
+    }, 200);
+    BigBrother.tickets.removeAll();
+    $.ajax({
+      url: "https://digitalcrew.teamwork.com/desk/v1/tickets/search.json",
+      method: "POST",
+      data: {
+        "search": '',
+        "searchType": "content",
+        "inboxIds": BigBrother.selectedInboxIds(),
+        "assignedTo": BigBrother.selectedUserIds().length ? BigBrother.selectedUserIds() : BigBrother.allUserIds(),
+        "pageSize": $("#ticketCount").val(),
+        "page": 1,
+        "sortBy": "updatedAt",
+        "sortDir": "asc",
+        "statuses": ["solved", "closed"],
+        "lastUpdated": $("#startDate").val()
+      },
+      headers: {
+        "Authorization": "Basic " + btoa(APIKey + ":xxx")
       }
-    };
-    self.selectUser = function(user) {
-      $("#users a[data-id='" + user.id + "']").toggleClass("selected");
-      if (self.selectedUsers.indexOf(user) > -1) {
-        return self.selectedUsers.remove(user);
-      } else {
-        return self.selectedUsers.push(user);
-      }
-    };
-    self.filterTickets = function() {
-      $(".placeholder, .tickets, .no-results").stop().animate({
+    }).done(function(data) {
+      var mappedTickets;
+      $(".loader").stop().animate({
         opacity: 0
       }, 200, function() {
         return $(this).hide();
       });
-      $(".loader").stop().show().animate({
-        opacity: 1
-      }, 200);
-      BigBrother.tickets.removeAll();
-      $.ajax({
-        url: "https://digitalcrew.teamwork.com/desk/v1/tickets/search.json",
-        method: "POST",
-        data: {
-          "search": '',
-          "searchType": "content",
-          "inboxIds": BigBrother.selectedInboxIds(),
-          "assignedTo": BigBrother.selectedUserIds().length ? BigBrother.selectedUserIds() : BigBrother.allUserIds(),
-          "pageSize": $("#ticketCount").val(),
-          "page": 1,
-          "sortBy": "updatedAt",
-          "sortDir": "asc",
-          "statuses": ["solved", "closed"],
-          "lastUpdated": $("#startDate").val()
-        },
-        headers: {
-          "Authorization": "Basic " + btoa(APIKey + ":xxx")
-        }
-      }).done(function(data) {
-        var mappedTickets;
-        $(".loader").stop().animate({
-          opacity: 0
-        }, 200, function() {
-          return $(this).hide();
+      if (data.tickets.length === 0) {
+        $(".no-results").stop().show().animate({
+          opacity: 1
+        }, 200);
+      } else {
+        mappedTickets = $.map(data.tickets, function(ticket) {
+          var agent, avatar;
+          agent = ticket.assignedTo ? ticket.assignedTo.firstName + " " + ticket.assignedTo.lastName : "None";
+          avatar = ticket.assignedTo ? $(".filters a[data-id='" + ticket.assignedTo.id + "'] img").attr("src") : null;
+          ticket = new Ticket(ticket.id, agent, ticket.preview, ticket.subject, ticket.status, ticket.updatedAt, avatar);
+          BigBrother.tickets.push(ticket);
         });
-        if (data.tickets.length === 0) {
-          $(".no-results").stop().show().animate({
-            opacity: 1
-          }, 200);
-        } else {
-          mappedTickets = $.map(data.tickets, function(ticket) {
-            var agent, avatar;
-            agent = ticket.assignedTo ? ticket.assignedTo.firstName + " " + ticket.assignedTo.lastName : "None";
-            avatar = ticket.assignedTo ? $(".filters a[data-id='" + ticket.assignedTo.id + "'] img").attr("src") : null;
-            ticket = new Ticket(ticket.id, agent, ticket.preview, ticket.subject, ticket.status, ticket.updatedAt, avatar);
-            BigBrother.tickets.push(ticket);
-          });
-          $(".tickets").stop().show().animate({
-            opacity: 1
-          }, 200);
-        }
-      });
-    };
-    self.reviewTickets = function(form) {
-      var body;
-      body = "<h5>What was done well?</h5>";
-      body += "<p>" + ($(form).find(".donewell").val()) + "</p>";
-      body += "<h5>Would you like to suggest any improvements?</h5>";
-      body += "<p>" + ($(form).find(".improvements").val()) + "</p>";
-      body += "<h5>Rating out of 5:</h5>";
-      body += "<p>" + ($(form).find(".rating input:checked").val()) + "</p>";
-      $(form).closest(".ticket").addClass("loading");
-      $.ajax({
-        url: "https://digitalcrew.teamwork.com/desk/v1/tickets/" + ($(form).find('.id').val()) + ".json",
-        method: "POST",
-        data: {
-          "type": "note",
-          "body": body
-        },
-        headers: {
-          "Authorization": "Basic " + btoa(APIKey + ":xxx")
-        }
-      }).done(function(data) {
-        $(form).closest(".ticket").removeClass("loading").addClass("success");
-      });
-    };
+        $(".tickets").stop().show().animate({
+          opacity: 1
+        }, 200);
+      }
+    });
   };
-  BigBrother = new FilterViewModel();
-  BigBrother.selectedInboxes.subscribe(function(inboxes) {
-    var ids;
-    ids = [];
-    $(inboxes).each(function() {
-      var inbox;
-      inbox = this;
-      return ids.push(inbox.id);
+  self.reviewTickets = function(form) {
+    var body;
+    body = "<h5>What was done well?</h5>";
+    body += "<p>" + ($(form).find(".donewell").val()) + "</p>";
+    body += "<h5>Would you like to suggest any improvements?</h5>";
+    body += "<p>" + ($(form).find(".improvements").val()) + "</p>";
+    body += "<h5>Rating out of 5:</h5>";
+    body += "<p>" + ($(form).find(".rating input:checked").val()) + "</p>";
+    $(form).closest(".ticket").addClass("loading");
+    $.ajax({
+      url: "https://digitalcrew.teamwork.com/desk/v1/tickets/" + ($(form).find('.id').val()) + ".json",
+      method: "POST",
+      data: {
+        "type": "note",
+        "body": body
+      },
+      headers: {
+        "Authorization": "Basic " + btoa(APIKey + ":xxx")
+      }
+    }).done(function(data) {
+      $(form).closest(".ticket").removeClass("loading").addClass("success");
     });
-    BigBrother.inboxCount(ids.length + " selected");
-    return BigBrother.selectedInboxIds(ids);
+  };
+};
+
+BigBrother = new FilterViewModel();
+
+BigBrother.selectedInboxes.subscribe(function(inboxes) {
+  var ids;
+  ids = [];
+  $(inboxes).each(function() {
+    var inbox;
+    inbox = this;
+    return ids.push(inbox.id);
   });
-  BigBrother.selectedUsers.subscribe(function(users) {
-    var ids;
-    ids = [];
-    $(users).each(function() {
-      var user;
-      user = this;
-      return ids.push(user.id);
-    });
-    BigBrother.userCount(users.length + " selected");
-    return BigBrother.selectedUserIds(ids);
+  BigBrother.inboxCount(ids.length + " selected");
+  return BigBrother.selectedInboxIds(ids);
+});
+
+BigBrother.selectedUsers.subscribe(function(users) {
+  var ids;
+  ids = [];
+  $(users).each(function() {
+    var user;
+    user = this;
+    return ids.push(user.id);
   });
+  BigBrother.userCount(users.length + " selected");
+  return BigBrother.selectedUserIds(ids);
+});
+
+init = function(APIKey) {
   $.ajax({
     url: "https://digitalcrew.teamwork.com/desk/v1/me.json",
     headers: {
@@ -167,6 +173,11 @@ init = function(APIKey) {
     BigBrother.meName(data.user.firstName + " " + data.user.lastName);
     BigBrother.meEmail(data.user.email);
     BigBrother.meAvatar(data.user.avatarURL);
+    $(".login").hide();
+    localStorage.setItem("APIKey", $("#APIKey").val());
+    ko.applyBindings(BigBrother);
+  }).fail(function() {
+    return $(".login p").text("Invalid API Key!");
   });
   $.ajax({
     url: "https://digitalcrew.teamwork.com/desk/v1/inboxes.json",
@@ -195,7 +206,7 @@ init = function(APIKey) {
     });
     BigBrother.users(mappedUsers);
   });
-  $(".filters h4").on("click", function() {
+  return $(".filters h4").on("click", function() {
     if ($(this).hasClass("open")) {
       return $(".filters h4").removeClass("open");
     } else {
@@ -203,14 +214,11 @@ init = function(APIKey) {
       return $(this).addClass("open");
     }
   });
-  return ko.applyBindings(BigBrother);
 };
 
 $("#login-form").on("submit", function(e) {
   e.preventDefault();
   init($("#APIKey").val());
-  $(".login").hide();
-  localStorage.setItem("APIKey", $("#APIKey").val());
 });
 
 if (localStorage.getItem("APIKey")) {
